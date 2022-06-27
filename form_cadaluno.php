85c3c3265733423ba99b857887aeeae41fd2bee8
Debora <106259609+deborabasilio@users.noreply.github.com>
2022-06-27 09:24:46 -0300

    Update form_cadaluno.php

--- form_cadaluno.php
+++ form_cadaluno.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadstro de Alunos</title>
-    
-    <link rel = "stylesheet" href = "estilos.css">
+
+    <link rel="stylesheet" href="estilos.css">
 
 </head>
-    <body>
-        <h1>Cadastro de Alunos</h1>
-        <form method = "POST" action = "insere_aluno.php">
-            <p><label for = "nome">Nome do Aluno:</label></p>
-            <p><input type = "text" name = "nome" id = "nome" /></p>
 
-            <p><label for = "data">Data de Nascimento:</label></p>
-            <p><input type = "date" name = "data" id = "data" /></p>
+<body>
+    <h1>Cadastro de Alunos</h1>
+    <form method="POST" action="insere_aluno.php">
+        <label for="nome">Nome do aluno:</label>
+        <input type="text" name="nome" id="nome" />
+        <label for="data">Data de Nascimento:</label>
+        <input type="date" name="data" id="data" />
+        <label for="cidade">Cidade:</label>
+        <select name="cidade">
+            <?php
+            $con = new mysqli("localhost", "root", "", "vinicio");
+            $sql = "SELECT codigo_cid, nome_cid, uf_cid 
+                        FROM cidades ORDER BY nome_cid";
+            $result = $con->query($sql);
+            while ($linha = $result->fetch_assoc()) {
+                echo "<option value=\"{$linha['codigo_cid']}\">{$linha['nome_cid']} - {$linha['uf_cid']}</option>";
+            }
+            ?>
+        </select>
 
-            <p><label for = "cidade">Cidade:</label></p>
-            <p><input type = "text" name = "cidade" id = "cidade" /><br/></p>
 
-            <p><input type = "submit" value = "Cadastrar" /></p>
-        </form>
-    </body>
+        <input type="submit" value="Cadastrar" />
+    </form>
+</body>
+
 </html>
